494315bee5810a73132cbfefaf2dc041f2e86308
TruptimayeePanigrahy <panigrahydeepakkumar27@gmail.com>
2023-06-16 20:27:47 +0530

    updatesomeroutes

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,18 @@
 const express = require("express");
 const { connection, client } = require("./config/db");
-const { logger } = require("./middlewares/logger.middleware");
-const { userRoute } = require("./routes/user.routes");
-const swaggerJSDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
-const { BookingRouter } = require("./routes/booking.routes")
-const { authRoute } = require("./routes/auth.routes");
+
+const { userRoute } = require("./routes/user.route");
+
+
+const { BookingRouter } = require("./routes/booking.route")
+const {adminrouter}=require("./routes/admin.route")
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(logger);
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
+app.use("/admin",adminrouter)
+
 
 app.get("/", (req, res) => {
   try {
@@ -23,36 +21,12 @@ app.get("/", (req, res) => {
     res.send({ "ok": false, "msg": error.message })
   }
 })
-app.use("/user", userRoute);
-const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Node JS API Project for BookMyShoot',
-      version: '1.0.0',
-      description:
-        "About : - This is a Photographer Booking application in which you can hire Top quality Photographers or become a Photographer and this is documentation of application BookMyShoot.",
-      license: {
-        name: "BookMyShoot"
-      },
-      contact: {
-        name: "BookMyShoot",
-        url: "bookmyshoot.com",
-        email: "kanj@gmail.com",
-      },
-    },
-    servers: [
-      {
-        url: 'http://localhost:3000/'
-      }
-    ]
-  },
-  apis: ['./routes/*.js']
-}
-const swaggerSpec = swaggerJSDoc(options)
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
-app.use("/auth", authRoute);
-app.use("/book", BookingRouter);
+// app.use("/user", userRoute);
+
+
+
+// app.use("/auth", authRoute);
+// app.use("/book", BookingRouter);
 app.listen(process.env.PORT, async () => {
   try {
     await connection;
