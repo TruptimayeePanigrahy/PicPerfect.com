74163785860623405a9ff40541b76e70317903aa
Shashwat2104 <Shashwatmahender2104@gmail.com>
2023-06-18 21:56:06 +0530

    Backend

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,24 +1,25 @@
 const jwt =require('jsonwebtoken')
 const {UserModel} = require("../models/user.model") 
 require("dotenv").config()
-const authMiddleWare = async(req,res,next)=>{
-    try {
-        const token = req.headers.authorization;
-        const decodedToken = jwt.verify(token,process.env.PRIVATE_KEY)
-        const {userId} = decodedToken;
-        //Checking if user exists
-        const user = await UserModel.findById(userId);
-        if(!user){
-            return res.status(401).json({message:"Please Login Again",ok:false})
-        }
-        if(user.isBlocked){
-            return res.status(403).json({message: 'Your account has been Blocked', ok:false})
-        }
-        req.user = user;
-        next()
-    } catch (error) {
-        return res.status(401).json({message:error.message})
+const authMiddleWare = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1]; // get token from Authorization header
+    const decodedToken = jwt.verify(token, process.env.secret);
+    const { userId } = decodedToken;
+    //Checking if user exists
+    const user = await UserModel.findById(userId);
+    if (!user) {
+      return res.status(401).json({ message: "Please Login Again", ok: false });
     }
-}
-
+    if (user.isBlocked) {
+      return res
+        .status(403)
+        .json({ message: "Your account has been Blocked", ok: false });
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: error.message });
+  }
+};
 module.exports = {authMiddleWare}
