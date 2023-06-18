d1b8a0f691915ab16201991895029c42f5ce3c4a
Shashwat2104 <Shashwatmahender2104@gmail.com>
2023-06-18 15:06:16 +0530

    Admin

--- frontend/Scripts/signup.js
+++ frontend/Scripts/signup.js
@@ -1,57 +1,62 @@
 const form = document.querySelector("form");
-const URL = ""
+const URL = "http://localhost:8185";
 
-const signup = document.getElementById("submit");  
-form.addEventListener("submit", async(e)=>{
-    e.preventDefault();
-    const formData = {
-        name:form.name.value,
-        email:form.email.value,
-        password:form.password.value,
-        role:formData.role.value
-    }
-    signup.style.display = "none";
-    showLoader2();
 
-    const request = await fetch(`${URL}/user/register`, {
-        method:"POST",
-        headers:{
-            "Content-type": "application/json"
-        },
-        body:JSON.stringify(formData)
+const signup = document.getElementById("submit");
+const client_checked = document.getElementById("radio1");
+const photographer_checked = document.getElementById("radio2");
+console.log("hello");
+form.addEventListener("submit", async (e) => {
+    console.log("change");
+  const checkedValue = document.querySelector(
+    "input[type=radio]:checked"
+  ).value;
+  e.preventDefault();
+  const formData = {
+    name: form.name.value,
+    email: form.email.value,
+    pass: form.pass.value,
+    role: checkedValue,
+  };
+    console.log(formData, "formdata1");
+  signup.style.display = "none";
+  showLoader2();
+
+  const request = await fetch(`${URL}/user/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  });
+  console.log(formData,"formdata2");
+  const response = await request.json();
+  console.log(response,"response");
+
+  if (response.ok) {
+    Swal.fire(response.msg, "", "success");
+    setTimeout(() => {
+      window.location.href = "../HTML/login.html";
+    }, 2500);
+  } else {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: response.msg,
+      footer: `<b><u><a href="../HTML/login.html">Login Here!</a></u></b>`,
     });
-    const response = await request.json();
-        
-    if(response.ok){
-        Swal.fire(
-            response.msg,
-            '',
-            'success'
-        )
-        setTimeout(()=>{
-            window.location.href = "../index.html";
-
-        },2500)
-    } else {
-        Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: response.msg,
-            footer: `<b><u><a href="../HTML/login.html">Login Here!</a></u></b>`
-        });
-    }
-    hideLoader2();
-    signup.style.display = "block";
-})
-
+  }
+  hideLoader2();
+  signup.style.display = "block";
+});
 
 const google = document.getElementById("google");
 const github = document.getElementById("github");
 
-google.addEventListener("click", ()=>{
-    window.location.href = "https://bookmyshoot-backend.onrender.com/auth/google"
-})
+google.addEventListener("click", () => {
+  window.location.href = "https://bookmyshoot-backend.onrender.com/auth/google";
+});
 
-github.addEventListener("click", ()=>{
-    window.location.href = "https://bookmyshoot-backend.onrender.com/auth/github"
-})
+github.addEventListener("click", () => {
+  window.location.href = "https://bookmyshoot-backend.onrender.com/auth/github";
+});
